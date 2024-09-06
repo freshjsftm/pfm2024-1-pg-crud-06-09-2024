b18c8519c90b8c87a5d9f87b21fc9dbd974e6e40
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-06 12:03:13 +0300

    Add methods CRUD at Thing

--- models/Thing.js
+++ models/Thing.js
@@ -1,17 +1,77 @@
 class Thing {
   static client = null;
   static tableName = 'things';
+  static attributes = {
+    title: 'string',
+  };
 
-  static async create(){
-   
+  static async create(values) {
+    const insertAttrs = Object.entries(this.attributes)
+      .filter(([nameAttr, valAttr]) => nameAttr in values)
+      .map(([nameAttr, valAttr]) => nameAttr);
+    const strInsertAttrs = insertAttrs
+      .map((nameAttr) => `"${nameAttr}"`)
+      .join(',');
+    const strInsertValues = insertAttrs
+      .map((nameAttr) => {
+        const value = values[nameAttr];
+        return typeof value === 'string' ? `'${value}'` : value;
+      })
+      .join(',');
+    const { rows } = this.client.query(`
+      INSERT INTO ${this.tableName}(${strInsertAttrs}) VALUES(${strInsertValues});
+    `);
+    return rows;
   }
 
-  static async findByPk(){}
-  static async findAll(){}
+  static async findByPk(pk) {
+    const { rows } = await this.client.query(`
+      SELECT * 
+      FROM ${this.tableName}
+      WHERE "id"=${pk};
+    `);
+    return rows;
+  }
 
-  static async updateByPk(){}
+  static async findAll() {
+    const { rows } = await this.client.query(`
+      SELECT * 
+      FROM ${this.tableName};
+    `);
+    return rows;
+  }
 
-  static async deleteByPk(){}
+  static async updateByPk(pk, values) {
+    const insertAttrs = Object.entries(this.attributes)
+      .filter(([nameAttr, valAttr]) => nameAttr in values)
+      .map(([nameAttr, valAttr]) => nameAttr);
+
+    const strSet = insertAttrs
+      .map((nameAttr) => {
+        const value = values[nameAttr];
+        const strValue = typeof value === 'string' ? `'${value}'` : value;
+        return `"${nameAttr}"=${strValue}`;
+      })
+      .join(',');
+
+    const { rows } = await this.client.query(`
+      UPDATE ${this.tableName} 
+      SET ${strSet}, "updatedAt"='${new Date().toISOString()}' 
+      WHERE id=${pk} 
+      RETURNING *;      
+      `);
+
+    return rows;
+  }
+
+  static async deleteByPk(pk) {
+    const { rows } = await this.client.query(`
+      DELETE FROM ${this.tableName}
+      WHERE "id"=${pk}
+      RETURNING *;
+    `);
+    return rows;
+  }
 }
 
 module.exports = Thing;
