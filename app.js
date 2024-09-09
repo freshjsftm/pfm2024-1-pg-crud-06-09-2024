059f833db77e7f5abd012805e2c8985b4006ab16
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-09 11:23:57 +0300

    Add get all & one

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const { createThing } = require('./controllers/thing.controller');
+const {
+  createThing,
+  getAllThings,
+  getThingByPk,
+  updateThingByPk,
+  deleteThingByPk,
+} = require('./controllers/thing.controller');
 const app = express();
 
 app.use(express.json());
 
-// routing 
+// routing
 app.post('/things', createThing);
+app.get('/things', getAllThings);
+
+app
+  .route('/things/:idThing')
+  .get(getThingByPk)
+  .put(updateThingByPk)
+  .delete(deleteThingByPk);
 
 module.exports = app;
--- controllers/thing.controller.js
+++ controllers/thing.controller.js
@@ -3,12 +3,51 @@ const { Thing } = require('../models');
 module.exports.createThing = async (req, res, next) => {
   try {
     const { body } = req;
-    const [newThing] = await Thing.create(body);   
-    if(newThing){
-      return res.status(201).send({data: newThing});
+    const [newThing] = await Thing.create(body);
+    if (newThing) {
+      return res.status(201).send({ data: newThing });
     }
     //bad practice
-    return res.status(400).send({data: 'Bad request'});
+    return res.status(400).send({ data: 'Bad request' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getAllThings = async (req, res, next) => {
+  try {
+    const arrayOfThings = await Thing.findAll();
+    if (arrayOfThings.length) {
+      return res.status(200).send({ data: arrayOfThings });
+    }
+    return res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getThingByPk = async (req, res, next) => {
+  try {
+    const {
+      params: { idThing },
+    } = req;
+    const [thing] = await Thing.findByPk(idThing);
+    if (thing) {
+      return res.status(200).send({ data: thing });
+    }
+    return res.status(404).send(); //bad practice
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.updateThingByPk = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.deleteThingByPk = async (req, res, next) => {
+  try {
   } catch (error) {
     next(error);
   }
